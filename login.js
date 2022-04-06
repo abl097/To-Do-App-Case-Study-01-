975761219a8aa50b3c8520565592625b97ec38b0
ARUN B L <90403019+abl097@users.noreply.github.com>
2022-04-06 22:02:19 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,38 @@
+var user = document.getElementById("typeUser");
+var pass = document.getElementById("typePassword");
+var errorMsg = document.getElementById("error")
+
+function validate(callback){
+    if(user.value==""){
+        errorMsg.innerHTML = "Username cannot be empty";
+        user.style.border = "2px solid red";
+    }
+    else if(pass.value==""){
+        errorMsg.innerHTML = "Password cannot be empty";
+        pass.style.border = "2px solid red";
+    }
+    else if(user.value=="admin" && pass.value=="12345"){
+        callback();
+    }
+    else{
+        errorMsg.innerHTML = "Invalid login details";
+    }
+}
+
+function loginfn(){
+    window.location.href = "./index.html";
+}
+
+// --------error message reset----------//
+
+function display(){
+    if(user.value!=" "){
+        user.style.border = "2px solid white";
+        errorMsg.innerHTML = "Enter username and password";
+    }
+    if(pass.value!=" "){
+        pass.style.border = "2px solid white";
+        errorMsg.innerHTML = "Enter username and password";
+    }
+    
+}
---
+++ todo.js
@@ -0,0 +1,52 @@
+Ajax();
+
+function Ajax(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4&&this.status==200){
+            var response = JSON.parse(this.responseText);
+            var output = "";
+            for(var i = 0;i<response.length;i++){
+                if (response[i].completed == true) {
+                  output += `<li class="list-group-item list list-group-item-primary"><input type="checkbox" checked disabled><span class="ms-3">${response[i].title}<span></li>`;     
+              } else {
+                  output += `<li class="list-group-item"><input class="form-check-input me-1" type="checkbox" value="" aria-label="..."  onchange="return taskDone();"><span class="ms-2">${response[i].title}<span></li>`;
+              }
+    
+            }
+            document.getElementById("content").innerHTML = output;
+        }
+    }
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+    xhttp.send();
+}
+
+// --------------Task completion alert-----------//
+
+function taskDone(){
+    return new Promise((resolve) => {
+            setTimeout(() => {
+                    var checkbox = document.getElementsByClassName("form-check-input");
+                    var done = 0;
+                    for (var i = 0; i < checkbox.length; i++) {
+                        if (checkbox[i].checked == true) {
+                            done += 1;
+                        }
+                    }
+                    if (done == 5) {
+                        resolve(alert("Congrats 5 Tasks have been successfully completed."));
+                    }
+                }, 50);
+        })
+}
+// --------------Task completion alert-----------//
+
+// ---------Reset button--------//
+var reset = document.getElementById("toDo");
+
+function resetTask(){
+    reset.innerHTML = "Reset Tasks";
+}
+function unSet(){
+    reset.innerHTML = "To-Do App"
+}
